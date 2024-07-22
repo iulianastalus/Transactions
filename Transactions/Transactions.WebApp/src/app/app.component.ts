0c39fbfe4a8fe5f992eef7aaa15e4bd06e595203
Iulian <iulian.astalus@gmail.com>
2024-07-22 16:43:38 +0300

    Implement Web App

---
+++ Transactions/Transactions.WebApp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { TransactionsService } from './transactions.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Transactions.WebApp';
+  errorMessage = "";
+  constructor(private transactionsService: TransactionsService) { }
+
+  checkFileFormat(fileEvent: Event) {
+    const target = fileEvent.target as HTMLInputElement;
+    const file: File = target.files![0];
+    if (file) {
+      if (file.type != "text/csv" && file.type != "text/xml") {
+        this.errorMessage = "Invalid Format !"
+      } else {
+        this.transactionsService.uploadFile(file).subscribe()
+      }
+    }
+  }
+}
