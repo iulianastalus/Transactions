2f609dc8a68e2dbd7d11caf7408b59f1f05de74f
Iulian <iulian.astalus@gmail.com>
2024-07-23 09:18:28 +0300

    Add Validator for TransactionCommand

--- Transactions/Transactions.Application/Commands/AddTransactionValidator.cs
+++ Transactions/Transactions.Application/Commands/AddTransactionValidator.cs
@@ -4,4 +4,12 @@ namespace Transactions.Application.Commands;
 
 public class AddTransactionValidator :AbstractValidator<AddTransactionCommand>
 {
+   public AddTransactionValidator()
+   {
+        RuleFor(pr => pr.TransactionId).NotNull().NotEmpty();
+        RuleFor(pr => pr.TransactionDate).NotNull().NotEmpty();
+        RuleFor(pr => pr.Amount).NotNull().NotEmpty();
+        RuleFor(pr => pr.CurrencyCode).NotNull().NotEmpty();
+        RuleFor(pr => pr.Status).NotNull().NotEmpty();
+    }
 }
